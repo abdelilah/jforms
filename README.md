4691ef367f6eaa28db5d0446094a063cc6c2011a
Abdelilah Sawab <airslim@gmail.com>
2015-04-03 17:02:52 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,164 @@
-# jforms
+# jForms
+
+A package to create Bootstrap compatible forms with validation and filtering for NodeJS.
+
+You can use this package with the ExpressJS framework or any other framework of your choice.
+
+For validation and filtering, you can use the **validator** (npm install validator) package, it comes with several handy methods. You can also use your own functions of course.
+
+This package was build in a quick and dirty way to suit my needs for several projects, and it's still a great way to get started. Any improvements or suggestions are welcome.
+
+## Installation 
+
+
+```
+npm install jforms
+```
+
+
+## Using jForms
+
+
+```javascript
+var validator = require('validator'); // Not required but preferred since it comes with many validation functions
+var jForms = require('jforms');
+
+// Create the form
+var myForm = new jForms({
+	firstname: {
+		label: 'firstname'
+	},
+	email: {
+		label: "Email",
+		required: true,
+		filters: [
+			validator.trim, 
+			function(val){ // Custom filter
+				return val.toLowerCase();
+			}
+		],
+		validators: [
+			validator.isEmail
+		]
+	},
+	submit: {
+		value: "Send",
+		icon: "fa fa-check",
+		className: "btn btn-primary"
+	}
+});
+
+// Handle data
+var data = { // Some dummy data to test against
+	firstname: 'John Doe',
+	email: 'john@doe.com'
+};
+myForm.handle(data, function(errors, filteredValues){
+	if(errors){
+		console.log("Invalid data");
+	}
+});
+```
+
+## Displaying the form
+jForms has the **toString** method defined so you can just print out your form variable to display the form.
+
+```
+console.log(myForm);
+// or
+console.log(myForm.toString());
+```
+
+
+## Full example
+```
+var v = require('validator');
+var jForms = require('jforms');
+
+var formJSON = {
+  firstname: {
+    label: "First name",
+    required: true,
+    validators: [v.isAlpha],
+    filters: [v.trim],
+    message: "Please enter your first name",
+    placeholder: "Your full name"
+  },
+  lastname: {
+    label: "Last name",
+    required: true,
+    message: "Please enter your last name"
+  },
+  password: {
+    label: "Password",
+    type: "password",
+    required: true
+  },
+  message: {
+    label: 'Message',
+    value: 'Hello World',
+    type: 'textarea',
+    filters: [v.trim],
+    rows: 10
+  },
+  gender: {
+    label: 'Gender',
+    type: 'radio',
+    value: 'f',
+    options: {
+      m: 'Male',
+      f: 'Female',
+    }
+  },
+  color: {
+    label: 'Color',
+    type: 'checkbox',
+    value: ['red', 'blue'],
+    options: {
+      red: 'Red',
+      green: 'Green',
+      blue: 'Blue',
+    }
+  },
+  region: {
+    label: 'City',
+    type: 'select',
+    value: 'ny',
+    name: "city", // This will override the key used for this field
+    options: {
+      '': 'Select your region',
+      ny: 'New York',
+      cal: 'California',
+    }
+  },
+  submit: {
+    value: "Send message",
+    className: "btn btn-primary",
+    icon: 'fa fa-check'
+  }
+};
+
+var myForm = new jForms(formJSON);
+
+myForm.enctype = "multipart/form-data";
+myForm.method = "post";
+myForm.className = "form-horizontal";
+myForm.leftSize = "col-sm-2"; // Size for label in horizontal forms
+myForm.rightSize = "col-sm-10"; // Size for field in horizontal forms
+myForm.id = "myForm";
+
+console.log("Is form valid ? " + myForm.isValid); // Useless before calling 'handle' method
+console.log("Was the form submitted ? " + myForm.isSubmitted);
+
+myForm.handle(req.body, function(errors, values){
+	if(errors){
+		console.log("Invalid form");
+	}
+	else{
+		// Save data maybe ?
+	}      
+});
+
+
+
+```
---
+++ index.js
@@ -0,0 +1,281 @@
+var _ = require('lodash');
+var randomstring = require("randomstring");
+
+
+function jForms(formFields){
+  this.fields = formFields;
+  this.enctype = '';
+  this.method = 'post';
+  this.action = '';
+  this.className = '';
+  this.leftSize = 'col-sm-2';
+  this.rightSize = 'col-sm-10';
+  this.id = '';
+  this.submit = {
+    icon: '', // Used to display an icon inside the button
+    value: 'Submit',
+    className: 'btn btn-primary'
+  };
+
+  this.values = {};
+  this.errors = {};
+  this.valuesFiltered = {};
+  this.isValid = true;
+  this.isSubmitted = false;
+};
+
+
+
+
+
+jForms.prototype.handle = function(data, callback){
+  var dataFiltered = {};
+  var errors = {};
+  var me = this;
+
+  me.isSubmitted = true;
+  me.values = data;
+
+
+  _.forEach(me.fields, function(field, fname){
+    if(field.name) fname = field.name; // Override field name if user has defined it
+    if(fname == "submit") return;
+
+    var message = "This field is invalid";
+    if(field.message) message = field.message;
+    var val = data[fname] ? data[fname] : null;
+
+    // Perform filter
+    if(field.filters && _.isArray(field.filters)){
+      _.forEach(field.filters, function(filter){
+        val = filter(val);
+      });
+    }
+
+    // Perform validation
+    if(field.required && field.required === true && (val == null || val == '')){ // Required state
+      errors[fname] = message;
+    }
+    if(field.validators && _.isArray(field.validators)){
+      _.forEach(field.validators, function(validator){
+        if(!validator(val)){
+          errors[fname] = message;
+        }
+      });
+    }
+    
+
+    dataFiltered[fname] = val;
+  });
+
+  // Finished validation
+  if(_.size(errors) < 1){
+    this.isValid = true;
+    errors = null;
+  }
+  else{
+    this.isValid = false;
+    this.errors = errors;
+  }
+  callback(errors, dataFiltered);
+}
+
+
+
+jForms.prototype.toString = function(){
+  var template = '';
+  var me = this;
+
+  // Function to add attributes to elements
+  var addAttribs = function(field){
+    // Anything else will be added as attributes to fields
+    var ignoreAttribs = [
+      'label', 
+      'type', 
+      'value', 
+      'validators', 
+      'filters', 
+      'message', 
+      'className', 
+      'options', 
+      'required',
+      'icon',
+      'name'
+    ];
+
+    // Add class name
+    if(!field.type || (field.type != "radio" && field.type != "checkbox")){
+      if(field.className) template += ' class="'+field.className+'"';
+      else template += ' class="form-control"';
+    }
+    
+
+    _.forEach(field, function(val, attr){
+      if(_.indexOf(ignoreAttribs, attr) > -1) return;
+      template += ' '+attr+'="'+val+'"';
+    });
+  };
+  
+  template += '<form action="'+this.action+'" method="'+this.method+'"';
+    if(this.enctype != '') template += ' enctype="'+this.enctype+'"';
+    if(this.className != '') template += ' class="'+this.className+'"';
+    if(this.id != '') template += ' id="'+this.id+'"';
+  template += '>';
+
+  
+
+  // Displaying fields
+  _.forEach(this.fields, function(field, fname){
+    if(fname == "submit") return;
+
+    if(!field.name){
+      field.name = fname;
+    }
+
+    if(!field.id){
+      field.id = 'form-'+field.name+'-'+randomstring.generate(8);
+    }
+
+    if(me.values[field.name]){
+      field.value = me.values[field.name];
+    }
+
+
+    template += '<div class="form-group';
+    if(me.isSubmitted === true){
+      template += me.errors[field.name] ? ' has-error' : ' has-success';
+    }
+    template += '">';
+
+    // Label
+    if(field.label && field.label != "") template += '<label class="control-label'+(me.className == "form-horizontal" ? ' '+me.leftSize : '')+'" for="'+field.id+'">'+field.label+'</label>';
+
+    if(me.className == "form-horizontal"){
+      template += '<div class="'+me.rightSize+'">';
+    }
+
+    // Text area
+    if(field.type && field.type == "textarea"){
+      template += '<textarea name="'+field.name+'"';
+        addAttribs(field);
+      template += '>';
+      if(field.value && field.value != "") template += field.value;
+      template += '</textarea>';
+    }
+    // Select
+    else if(field.type && field.type == "select"){
+      template += '<select name="'+field.name+'"';
+        addAttribs(field);
+      template += '>';
+        if(field.options){
+          _.forEach(field.options, function(val, key){
+            template += '<option value="'+key+'"'+(field.value && field.value == key ? ' selected="selected"' : '')+'>'+val+'</option>';
+          });
+        }
+      template += '</select>';
+    }
+    // Radio
+    else if(field.type && field.type == "radio"){
+        if(field.options){
+          _.forEach(field.options, function(val, key){
+            template += '<div class="radio">';
+              template += '<label>';
+                template += '<input type="radio" name="'+field.name+'" value="'+key+'"'+(field.value && field.value == key ? ' checked' : '');
+                addAttribs(field);
+                template += '>'+val;
+              template += '</label>';
+            template += '</div>';
+          });
+        }
+    }
+    // Checkbox
+    else if(field.type && field.type == "checkbox"){
+        if(field.options){
+          _.forEach(field.options, function(val, key){
+            var checked = [];
+            if(field.value && _.isArray(field.value))
+              checked = field.value;
+            else if(field.value && _.isString(field.value))
+              checked = field.value.split(',');
+
+            template += '<div class="checkbox">';
+              template += '<label>';
+                template += '<input type="checkbox" name="'+field.name+'" value="'+key+'"'+(_.indexOf(checked, key) > -1 ? ' checked' : '');
+                addAttribs(field);
+                template += '>'+val;
+              template += '</label>';
+            template += '</div>';
+          });
+        }
+    }
+    // Password
+    else if(field.type && field.type == "password"){
+      template += '<input type="password" name="'+field.name+'"';
+        if(field.value && field.value != "") template += ' value="'+field.value+'"';
+        addAttribs(field);
+      template += '>';
+    }
+    // Hidden
+    else if(field.type && field.type == "hidden"){
+      template += '<input type="hidden" name="'+field.name+'"';
+        if(field.value && field.value != "") template += ' value="'+field.value+'"';
+        addAttribs(field);
+      template += '>';
+    }
+    // Text input
+    else{
+      template += '<input type="text" name="'+field.name+'"';
+        if(field.value && field.value != "") template += ' value="'+field.value+'"';
+        addAttribs(field);
+      template += '>';
+    }
+    
+
+    if(me.errors[field.name]){
+      template += '<p class="help-block">'+me.errors[field.name]+'</p>';
+    }
+
+
+    if(me.className == "form-horizontal"){
+      template += '</div>';
+    }
+
+    template += '</div>';
+  });
+
+
+  // Submit button
+  var btnSubmit = this.fields.submit || this.submit;
+  if(btnSubmit){
+    btnSubmit.type = "submit";
+  }
+  template += '<div class="form-group form-submit">';
+    if(me.className == "form-horizontal"){
+      template += '<div class="'+me.leftSize+'">';
+      template += '</div>';
+      template += '<div class="'+me.rightSize+'">';
+    }
+    template += ' <button type="submit"';
+    addAttribs(btnSubmit);
+    template += '>';
+
+    if(btnSubmit.icon && btnSubmit.icon != ''){
+      template += '<i class="'+btnSubmit.icon+'"></i> ';
+    }
+
+    template +=     btnSubmit.value;
+    template += ' </button>';
+    if(me.className == "form-horizontal"){
+      template += '</div>';
+    }
+  template += '</div>';
+
+
+  template += '</form>';
+
+  
+  return template;
+}
+
+
+module.exports = jForms;
